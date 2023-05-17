2a09c617e6a24e6444af03ca803e7f7698de13ed
Psyhe <evarosa@interia.pl>
2023-05-17 21:41:16 +0200

    main_in_progress

--- src/main/java/elevatorsystem/Main.java
+++ src/main/java/elevatorsystem/Main.java
@@ -23,6 +23,34 @@ class Main {
     }
 
     public static void main (String[] args) {
+        ElevatorSystem elevatorSystem = new ElevatorSystem(16, 16);
         System.out.println("helloworld");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String line = scanner.nextLine();
+            String[] splitLine = line.split(" ");
+            // Process each line of the file here
+            System.out.println(splitLine[0]);
+            if (splitLine[0].equals("PICKUP")) {
+                System.out.println("PICKUP");
+            }
+            else if (splitLine[0].equals("STEP")) {
+                System.out.println("STEP");
+            }
+            else if (splitLine[0].equals("UPDATE")) {
+                System.out.println("UPDATE");
+            }
+            else if (splitLine[0].equals("STATUS")) {
+                System.out.println("STATUS");
+            }
+            else if(splitLine[0].equals("QUIT")) {
+                System.out.println("QUIT");
+                scanner.close();
+                break;
+            }
+            else {
+                System.out.println("Invalid command");
+            }
+        }
     }
 }
